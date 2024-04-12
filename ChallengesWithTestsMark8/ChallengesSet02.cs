74877724025c5f01d60ce0e1a62cefe6c1a93534
Hy Huynh <hyxhuynh.projects@gmail.com>
2024-04-12 00:58:38 -0700

    completed ChallengesSet02

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +8,114 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            string lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+            char[] lowercaseCharArr = lowercaseAlphabet.ToCharArray();
+            string uppercaseAlphabet = lowercaseAlphabet.ToUpper();
+            char[] uppercaseCharArr = uppercaseAlphabet.ToCharArray();
+            for (int i = 0; i < lowercaseCharArr.Length; i++)
+            {
+                if (c == lowercaseCharArr[i])
+                {
+                    return true;
+                }
+            }
+            for (int x = 0; x < uppercaseCharArr.Length; x++)
+            {
+                if (c == uppercaseCharArr[x])
+                {
+                    return true;
+                }
+            }
+            return false;
+
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if (vals.Length % 2 == 0) { return true; }
+            return false;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0) { return true; }
+            return false;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 != 0) { return true; }
+            return false;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null) { return 0; }
+            if (numbers.Count() == 0) { return 0; }
+            return numbers.Min() + numbers.Max();
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length < str2.Length)
+            {
+                return str1.Length;
+            }
+            else
+            {
+                return str2.Length;
+            }
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            if (numbers == null) { return 0; }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            if (numbers == null) { return 0; }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+            return sum;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            if (numbers == null) { return false;}
+            for (int i = 0; i < numbers.Count;i++)
+            {
+                sum += numbers[i];
+            }
+            if (sum % 2 != 0) { return true;}
+            else { return false;}
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            List<int> posOddList = new List<int>();
+
+            for (int i = 1; i < number; i++)
+            {
+                if (i > 0 && i % 2 != 0)
+                {
+                    posOddList.Add(i);
+                }
+            }
+            return posOddList.Count;
         }
     }
 }
