471bc50a8f587674dac2f141595e7b0f331fdd53
Hy Huynh <hyxhuynh.projects@gmail.com>
2024-04-26 03:49:12 -0700

    completed ChallengesSet04

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +8,116 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sumEvensSubtractOdds = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sumEvensSubtractOdds += numbers[i];
+                }
+                if (numbers[i] % 2 != 0)
+                {
+                    sumEvensSubtractOdds -= numbers[i];
+                }
+            }
+            return sumEvensSubtractOdds;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            int[] stringLength = {str1.Length, str2.Length, str3.Length, str4.Length};
+            return stringLength.Min();
+
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            int[] numbers = {number1, number2, number3, number4};
+            return numbers.Min();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            if (sideLength1 + sideLength2 > sideLength3
+                && sideLength1 + sideLength3 > sideLength2
+                && sideLength2 + sideLength3 > sideLength1)
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            decimal number;
+            bool isANumber = decimal.TryParse(input, out number);
+            if (isANumber)
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            int numberOfNulls = 0;
+            int arrayLength = objs.Length;
+            for (int i = 0; i < arrayLength; i++)
+            {
+                if (objs[i] == null)
+                {
+                    numberOfNulls++;
+                }
+                if (numberOfNulls > arrayLength - numberOfNulls)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            double sumOfEvens = 0;
+            double numOfEvens = 0;
+            for (int i=0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sumOfEvens = sumOfEvens + numbers[i];
+                    numOfEvens++;
+                }
+            }
+
+            if (numOfEvens == 0 || sumOfEvens == 0)
+            {
+                return 0;
+            }
+
+            return sumOfEvens / numOfEvens;
         }
 
         public int Factorial(int number)
-        {
-            throw new NotImplementedException();
+        {   
+            if (number == 0)
+            {
+                return 1;
+            }
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return number * Factorial(number -1);
         }
     }
 }
--- ChallengesWithTestsMark8/obj/Debug/net6.0/ChallengesWithTestsMark8.AssemblyInfo.cs
+++ ChallengesWithTestsMark8/obj/Debug/net6.0/ChallengesWithTestsMark8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74877724025c5f01d60ce0e1a62cefe6c1a93534")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0ded69ad826e08d555e5d605f9cfcad23703ea0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
